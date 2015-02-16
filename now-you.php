6cd72407a2da376ebe8a2e5fe324f219cf6510b1
Greg Goforth <ggoforth@shift3tech.com>
2015-02-16 14:01:52 -0800

    adds now-you.php

--- now-you.php
+++ now-you.php
@@ -1,11 +1,17 @@
 <?php
 
 // your mission, should you choose to accept it is to:
-// - create a new class, call it Vehicle
-// - make sure it has a constructor function that takes 4 arguments
+// - create a new class, call it Car
+// - make sure it has a constructor function that takes 3 arguments
 //    - Make
 //    - Model
-//    - Color
 //    - Year
-//    -
-// -
+// - Car should implement a drive function, that echos out "Car is driving!"
+// - create another class that extends Car called MyCar
+//    - It should use a constructor function and call to the parent::_construct class
+//    - It's constructor should also take one argument, an array (associative array to be exact)
+//    - Using that associative array, loop it's values and set them as properties of the class
+//    - It should implement a details function that will echo out the make, model, and year.
+// - Create an instance of the class
+// - Call the drive function
+// - Call the details function
